f9b987bd519a75257e7f95d90f7552dbe63fb62a
Hiroki Chiyoda <contact@arafipro.com>
2024-05-01 15:56:00 +0900

    10_create-table-comp

---
+++ app/components/todo-table.tsx
@@ -0,0 +1,32 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+export default function TodoTable({ todos }: { todos: Todo[] }) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>todo</TableHead>
+          <TableHead>score</TableHead>
+          <TableHead>isDone</TableHead>
+          <TableHead>createAt</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {todos.map((todo) => (
+          <TableRow key={todo.id}>
+            <TableCell>{todo.todo}</TableCell>
+            <TableCell>{todo.score}</TableCell>
+            <TableCell>{todo.isDone.toString()}</TableCell>
+            <TableCell>{todo.createAt}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import TodoTable from "./components/todo-table";
 
 export default async function Home() {
   const res = await fetch(
@@ -15,26 +8,7 @@ export default async function Home() {
   console.log(todos);
   return (
     <main className="">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>todo</TableHead>
-            <TableHead>score</TableHead>
-            <TableHead>isDone</TableHead>
-            <TableHead>createAt</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {todos.map((todo) => (
-            <TableRow key={todo.id}>
-              <TableCell>{todo.todo}</TableCell>
-              <TableCell>{todo.score}</TableCell>
-              <TableCell>{todo.isDone}</TableCell>
-              <TableCell>{todo.createAt}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <TodoTable todos={todos} />
     </main>
   );
 }
